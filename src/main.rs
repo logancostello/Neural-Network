29b93bbac819df38911f9edc4fdc013951c84251
Logan Costello <logancostello2022@gmail.com>
2024-09-04 18:01:37 -0700

    classification where weights can be manually set. linear decision boundary only

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+const weight_1_1: u32 = 5;
+const weight_2_1: u32 = 3;
+const weight_1_2: u32 = 7;
+const weight_2_2: u32 = 1;
+
+const bias_1: u32 = 3;
+const bias_2: u32 = 4;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("{}", classify(3, 2));   
+
+}
+
+fn classify(x: u32, y: u32) -> u32 {
+    let output1 = x * weight_1_1 + y * weight_2_1 + bias_1;
+    let output2 = x * weight_1_2 + y * weight_2_2 + bias_2;
+
+    if output1 >= output2 { return 0 } else { 1 }
+
 }
