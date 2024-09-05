75147bf3c87e0c277a80c06f0038dd334ddd43f1
Logan Costello <logancostello2022@gmail.com>
2024-09-05 00:33:31 -0700

    cleaned

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
 mod layer;
 mod neural_network;
 
-const WEIGHT_1_1: u32 = 5;
-const WEIGHT_2_1: u32 = 3;
-const WEIGHT_1_2: u32 = 7;
-const WEIGHT_2_2: u32 = 1;
-
-const BIAS_1: u32 = 3;
-const BIAS_2: u32 = 2;
-
 fn main() {
-
-    println!("{}", classify(0, 0));   
-
-}
-
-fn classify(x: u32, y: u32) -> u32 {
-    let output1 = x * WEIGHT_1_1 + y * WEIGHT_2_1 + BIAS_1;
-    let output2 = x * WEIGHT_1_2 + y * WEIGHT_2_2 + BIAS_2;
-
-    if output1 >= output2 { return 0 } else { 1 }
-
+    println!("training here soon");   
 }
--- target/debug/deps/Neural_Network-8dc10bdc9792b4ac.d
+++ target/debug/deps/Neural_Network-8dc10bdc9792b4ac.d
@@ -1,6 +1,7 @@
-/Users/logancostello/Projects/Neural-Network/target/debug/deps/Neural_Network-8dc10bdc9792b4ac: src/main.rs src/layer.rs
+/Users/logancostello/Projects/Neural-Network/target/debug/deps/Neural_Network-8dc10bdc9792b4ac: src/main.rs src/layer.rs src/neural_network.rs
 
-/Users/logancostello/Projects/Neural-Network/target/debug/deps/Neural_Network-8dc10bdc9792b4ac.d: src/main.rs src/layer.rs
+/Users/logancostello/Projects/Neural-Network/target/debug/deps/Neural_Network-8dc10bdc9792b4ac.d: src/main.rs src/layer.rs src/neural_network.rs
 
 src/main.rs:
 src/layer.rs:
+src/neural_network.rs:
