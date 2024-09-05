592c32efd78a931a41e612247389dd63fdcae0c7
Logan Costello <logancostello2022@gmail.com>
2024-09-05 00:32:45 -0700

    added activation function

--- src/layer.rs
+++ src/layer.rs
@@ -13,16 +13,21 @@ impl Layer {
 
     // Loop through all of the inputs and calculate the outputs
     pub fn calculate_outputs(&self, inputs: &Vec<f32>) -> Vec<f32> {
-        let mut outputs: Vec<f32> = Vec::new();
+        let mut activations: Vec<f32> = Vec::new();
         for i in 0..inputs.len() {
-            let mut output = self.biases[i];
+            let mut weighted_input = self.biases[i];
             for j in 0..self.weights.len() {
-                output += inputs[j] * self.weights[i][j];
+                weighted_input += inputs[j] * self.weights[i][j];
             }
-            outputs.push(output);
+            activations.push(activation_function(weighted_input));
         }
-        outputs
+        activations
     }
+}
+
+// Using the ReLU Activation Function
+fn activation_function(weighted_input: f32) -> f32 {
+    (weighted_input + weighted_input.abs()) / 2.0
 }
 
 #[cfg(test)]
@@ -31,12 +36,23 @@ mod tests {
 
     // Test two inputs to two outputs
     #[test]
-    fn test_calculate_outputs() {
+    fn test_calculate_outputs_1() {
         let weights: Vec<Vec<f32>> = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
         let biases: Vec<f32> = vec![5.0, 6.0];
         let layer = Layer::new(weights, biases);
         let inputs: Vec<f32> = vec![1.0, 2.0];
         let outputs = layer.calculate_outputs(&inputs);
         assert_eq!(outputs, vec![10.0, 17.0]);
+    } 
+
+    // Test activation function has effect
+    #[test]
+    fn test_calculate_outputs_2() {
+        let weights: Vec<Vec<f32>> = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+        let biases: Vec<f32> = vec![0.0, 0.0];
+        let layer = Layer::new(weights, biases);
+        let inputs: Vec<f32> = vec![-1.0, -2.0];
+        let outputs = layer.calculate_outputs(&inputs);
+        assert_eq!(outputs, vec![0.0, 0.0]);
     } 
 }
