184154a509422429c77fd418f716a3414479dd83
Logan Costello <logancostello2022@gmail.com>
2024-09-05 01:20:34 -0700

    implemented loss function

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -1,6 +1,7 @@
 use crate::layer::Layer;
+use crate::data_point::DataPoint;
 
-struct NeuralNetwork {
+pub struct NeuralNetwork {
     layers: Vec<Layer>
 }
 
@@ -11,17 +12,40 @@ impl NeuralNetwork {
         NeuralNetwork { layers }
     }
 
-    // Classify the inputs by running them through the network
-    pub fn classify(&self, inputs: &Vec<f32>) -> usize {
+    // Run the inputs through the network to get the outputs
+    pub fn calculate_outputs(&self, inputs: &Vec<f32>) -> Vec<f32> {
         let mut inputs_for_next_layer: Vec<f32> = inputs.clone();
         for layer in &self.layers {
             // The outputs of one layer are the inputs for the next layer
             inputs_for_next_layer = layer.calculate_outputs(&inputs_for_next_layer);
         }
-        // Indicate class by returning the index of the greatest output
-        inputs_for_next_layer.iter().enumerate().max_by(|&(_, a), &(_, b)| a.partial_cmp(b).unwrap()).map(|(index, _)| index).unwrap()
+        inputs_for_next_layer
+    }
+
+    // Calculate the loss for a given datapoint
+    pub fn loss(&self, data: &Vec<DataPoint>) -> f32 {
+        let mut loss: f32 = 0.0;
+
+        // Get loss for each DataPoint
+        for dp in data {
+            let outputs: Vec<f32> = self.calculate_outputs(&dp.inputs);
+            
+            // Add error of each node
+            for i in 0..outputs.len() {
+                let error = outputs[i] - dp.expected_outputs[i];
+                loss += error * error;
+            }
+        }
+
+        // Return average loss for consistency across varying amounts of data
+        loss / data.len() as f32
     }
 } 
+
+// Indicate class by returning the index of the greatest output
+pub fn classify(nn_outputs: &Vec<f32>) -> usize {
+    nn_outputs.iter().enumerate().max_by(|&(_, a), &(_, b)| a.partial_cmp(b).unwrap()).map(|(index, _)| index).unwrap()
+}
 
 
 #[cfg(test)]
@@ -36,6 +60,18 @@ mod tests {
         let layer = Layer::new(weights, biases);
         let network = NeuralNetwork::new(vec![layer]);
         let inputs: Vec<f32> = vec![1.0, 2.0];
-        assert_eq!(1, network.classify(&inputs));
+        assert_eq!(1, classify(&network.calculate_outputs(&inputs)));
     } 
+
+    // Test loss function
+    #[test]
+    fn test_loss_1() {
+        let data = vec![DataPoint::new(vec![0.0, 0.0], vec![0.0, 0.0])];
+        let weights: Vec<Vec<f32>> = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+        let biases: Vec<f32> = vec![5.0, 6.0];
+        let layer = Layer::new(weights, biases);
+        let network = NeuralNetwork::new(vec![layer]);
+
+        assert_eq!(network.loss(&data), 5.0 * 5.0 + 6.0 * 6.0);
+    }
 }
