8de2f996300e696bda5aee9f2e665ad80060ba67
Logan Costello <logancostello2022@gmail.com>
2024-09-05 01:20:20 -0700

    added new func

--- src/data_point.rs
+++ src/data_point.rs
@@ -3,3 +3,11 @@ pub struct DataPoint {
     pub inputs: Vec<f32>,
     pub expected_outputs: Vec<f32>
 }
+
+impl DataPoint {
+
+    // Create new DataPoint
+    pub fn new(inputs: Vec<f32>, expected_outputs:Vec<f32>) -> Self {
+        DataPoint { inputs, expected_outputs }
+    }
+}
