e204f2e7619cf3fd2e80d311ab70c44b4d523b98
Logan Costello <logancostello2022@gmail.com>
2024-09-05 01:08:08 -0700

    created DataPoint class for training

---
+++ src/data_point.rs
@@ -0,0 +1,5 @@
+// Data that the network can be trained on
+pub struct DataPoint {
+    pub inputs: Vec<f32>,
+    pub expected_outputs: Vec<f32>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod layer;
 mod neural_network;
+mod data_point;
 
 fn main() {
     println!("training here soon");   
