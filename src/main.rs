57cb6ec766e5c4bd27bf2c38441569bb3ab522c8
Logan Costello <logancostello2022@gmail.com>
2024-09-07 18:09:16 -0700

    neural network achieves 100% accuracy on 2d linear dataset

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,8 @@ use crate::neural_network::NeuralNetwork;
 use rand::Rng;
 
 fn main() {
-    // Test if neural network can learn to identify 2d points separated by a linear line
-    let layer = Layer::new(2, 2);
-    let mut neural_network = NeuralNetwork::new(vec![layer]);
-    let training_data = generate_linear_test(100, -1.0, 4.0);
-    println!("Original loss: {}", neural_network.loss(&training_data));
-    for i in 0..500 {
-        neural_network.learn(&training_data, 0.2);
-        println!("{i}: Updated loss: {}", neural_network.loss(&training_data));
-    }
+    let learn_rate = 2.5;
+    run_2d_linear_test(learn_rate);
 }
 
 // Generates n points with two populations, indicating whether they are above or below the given line
@@ -29,13 +22,27 @@ fn generate_linear_test(n: usize, slope: f32, intercept: f32) -> Vec<DataPoint> 
         let x = rng.gen_range(0.0..10.0);
         let y = rng.gen_range(0.0..10.0);
 
-        let expected_output: Vec<f32> = if y >= slope * x + intercept {
-            vec![1.0, 0.0]
+        let expected_output: Vec<usize> = if y >= slope * x + intercept {
+            vec![1, 0]
         } else {
-            vec![0.0, 1.0]
+            vec![0, 1]
         };
 
         training_data.push(DataPoint::new(vec![x, y], expected_output));
     }
     training_data
+}
+
+// Test if neural network can learn to identify 2d points separated by a linear line
+fn run_2d_linear_test(learn_rate: f32) {
+    let layer = Layer::new(2, 2);
+    let mut neural_network = NeuralNetwork::new(vec![layer]);
+    let training_data = generate_linear_test(100, -1.0, 4.0);
+    let mut num = 1;
+    println!("Loss: {}, Accuracy: {}", neural_network.loss(&training_data), neural_network.accuracy(&training_data));
+    while neural_network.accuracy(&training_data) < 1.0 {
+        neural_network.learn(&training_data, learn_rate);
+        println!("{num}: Loss: {}, Accuracy: {}", neural_network.loss(&training_data), neural_network.accuracy(&training_data));
+        num += 1;
+    }
 }
